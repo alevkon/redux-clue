bda612cbd2bbcb939c2f6b56c556049472cc0321
alevkon <alevkon@gmail.com>
2018-05-14 08:27:14 +0300

    Add saga

--- tests/reducer.test.js
+++ tests/reducer.test.js
@@ -28,7 +28,7 @@ describe("Reducer", () => {
         _requests: {
           [action.payload.requestId]: expectedItem
         },
-        user: {
+        [action.payload.clue.identity]: {
           [action.payload.clue.query]: {
             [action.payload.clue.serialize()]: expectedItem
           }
@@ -59,9 +59,9 @@ describe("Reducer", () => {
       };
       expect(state2.toJSON()).toMatchObject({
         _requests: {
-          [action1.payload.requestId]: expectedItem,
+          [action1.payload.requestId]: expectedItem
         },
-        user: {
+        [action1.payload.clue.identity]: {
           [action1.payload.clue.query]: {
             [action1.payload.clue.serialize()]: expectedItem
           }
@@ -69,6 +69,82 @@ describe("Reducer", () => {
       });
       expect(state2.user[action1.payload.clue.query].size).toEqual(1);
       expect(state2._requests.size).toEqual(1);
+    });
+  });
+
+  describe("Success event", () => {
+    test("Should remove from _requests and put data into [identity]", () => {
+      const clueData = {
+        identity: "user",
+        query: queries.FIND_BY_ID,
+        id: 1
+      };
+      const responseHeaders = { someHeader: "ok" };
+      const responseData = { someAttribute: "some value" };
+
+      const action1 = api.actions.byClue(clueData);
+      action1.payload.requestId = Math.random();
+      const action2 = api.actions.success(action1.payload.requestId, action1.payload.clue, responseHeaders, responseData);
+
+      const state1 = api.reducer(undefined, action1);
+      const state2 = api.reducer(state1, action2);
+
+      expect(state2.toJSON()).toMatchObject({
+        [action1.payload.clue.identity]: {
+          [action1.payload.clue.query]: {
+            [action1.payload.clue.serialize()]: {
+              pending: false,
+              error: false,
+              data: responseData,
+              meta: {
+                clue: action1.payload.clue,
+                fromResponse: responseHeaders
+              }
+            }
+          }
+        }
+      });
+      expect(state2._requests.size).toEqual(0);
+    });
+  });
+
+  describe("Error event", () => {
+    test("Should patch _requests and put data into [identity]", () => {
+      const clueData = {
+        identity: "user",
+        query: queries.FIND_BY_ID,
+        id: 1
+      };
+      const responseHeaders = { someHeader: "ok" };
+      const responseData = { someAttribute: "some value" };
+
+      const action1 = api.actions.byClue(clueData);
+      action1.payload.requestId = Math.random();
+      const action2 = api.actions.error(action1.payload.requestId, action1.payload.clue, responseHeaders, responseData);
+
+      const state1 = api.reducer(undefined, action1);
+      const state2 = api.reducer(state1, action2);
+
+      const expectedItem = {
+        pending: false,
+        error: true,
+        data: responseData,
+        meta: {
+          clue: action1.payload.clue,
+          fromResponse: responseHeaders
+        }
+      };
+
+      expect(state2.toJSON()).toMatchObject({
+        _requests: {
+          [action1.payload.requestId]: expectedItem
+        },
+        [action1.payload.clue.identity]: {
+          [action1.payload.clue.query]: {
+            [action1.payload.clue.serialize()]: expectedItem
+          }
+        }
+      });
     });
   });
 });
